c785220e9f9712fba8706498139b26d8eee0e24a
Farhan <fmaj0003@student.monash.edu>
2023-04-06 00:16:00 +1000

    Lab Report
    Completed the lab report

--- Week6_worksheet.R
+++ Week6_worksheet.R
@@ -5,37 +5,38 @@ library(JWileymisc)
 library(mice)
 library(VIM)
 library(ggplot2)
+library(haven)
 
 # Load in the daily diary dataset (multiple surveys per person)
-dd <- as.data.table(???("???"))
+dd <- as.data.table(read_sav("[2021] PSY4210 DD.sav")) 
 
 # When you have a lots of variables, it's hard to get the pattern plots
 # to look useful. 
 
 # Make a subset of dd called dd_sub that has these variables: 
 # SurveyDay, dSE, dMood, dStress, dEnergy, dLoneliness
-??? <- ???[, .(???)]
+dd_sub <- dd[, .(SurveyDay, dSE, dMood, dStress, dEnergy, dLoneliness)]
 
 # Now use the aggr() function on the subset to look at missing data patterns
 # Hint: if the proportions are not showing up on the right hand side, 
 # add this argument: 'cex.numbers = .8'. If variable labels don't show up,
 # add: 'cex.axis = .8'
 
-aggr(???)
+aggr(dd_sub, prop = TRUE, numbers = TRUE, cex.numbers = .5, cex.axis = .8)
 
 # Which variables don't have any missing data? 
 
-# A: 
+# A: SurveyDay, dMood, dStress, dEnergy
 
 # What percentage of the observations aren't missing any data?
 
-# A:  
+# A: 90%
 
 # Make a marginplot comparing missingness of dLoneliness to the values of dMood
-???
+marginplot(dd_sub[, .(dMood,dLoneliness)])
 
 # Does there seem to be much association?
-# A: 
+# A: No, not much association               ##Double check this with Michelle
 
 # To do MI, we have to make sure the class of all the variables are correct.
 # Because they were imported from SPSS, they're all wrong. How do we change them?
